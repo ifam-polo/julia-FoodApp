4ab0ff51814dbfccf2383ed32d9da84effce481c
Julia <donadellij@gmail.com>
2023-03-13 12:19:00 -0400

    criando views, models, html

--- customer/models.py
+++ customer/models.py
@@ -1,5 +1,28 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
+class MenuItem(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    img = models.ImageField(upload_to='menu_images/')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ManyToManyField('Category', related_name='item')
 
+    def __str__(self):
+        return self.nome
+
+class Category(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class OrderModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)  
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
+
+    def __str__(self):
+        return f'Order: {self.created.strftime("%b %d %I: %M %p")}'
--- customer/views.py
+++ customer/views.py
@@ -10,3 +10,24 @@ class Index(View):
 class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        
+        # get every item from each category/ pega todos os item de cada categoria
+        aperitivos = MenuItem.objects.filter(category__name__contains='aperitivos')
+        bebidas = MenuItem.objects.filter(category__name__contains='bebidas')
+        pratoP = MenuItem.objects.filter(category__name__contains='prato principal')
+        sobremesa = MenuItem.objects.filter(category__name__contains='sobremesa')
+        # pass into context/ passa para o context
+
+        context = {
+            'aperitivos': aperitivos,
+            'pratoP': pratoP,
+            'sobremesa': sobremesa,
+            'bebidas': bebidas,
+        }
+
+        # render the template/ carrega o template
+
+
